f92fcd344d0f418905981a18545c3456e91b2ba4
Hiroshi Urabe <mail@torounit.com>
2025-08-27 00:19:25 +0900

    feat: eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,25 @@
-// @ts-check
+import { includeIgnoreFile } from '@eslint/compat'
+import eslint from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import tseslint from 'typescript-eslint'
+import { fileURLToPath } from 'node:url'
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
-);
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
+  {
+    ignores: ['eslint.config.mjs']
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: fileURLToPath(new URL('.', import.meta.url)),
+      }
+    }
+  }
+)
