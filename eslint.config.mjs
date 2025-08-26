c765f3cc64e27da4460d40e53da8d5f2597d4cf0
Hiroshi Urabe <mail@torounit.com>
2025-08-27 00:19:51 +0900

    refactor: format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,25 @@
-import { includeIgnoreFile } from '@eslint/compat'
-import eslint from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import tseslint from 'typescript-eslint'
-import { fileURLToPath } from 'node:url'
+import { includeIgnoreFile } from "@eslint/compat";
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+import { fileURLToPath } from "node:url";
 
-const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   eslintConfigPrettier,
-  includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
+  includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
   {
-    ignores: ['eslint.config.mjs']
+    ignores: ["eslint.config.mjs"],
   },
   {
     languageOptions: {
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: fileURLToPath(new URL('.', import.meta.url)),
-      }
-    }
-  }
-)
+        tsconfigRootDir: fileURLToPath(new URL(".", import.meta.url)),
+      },
+    },
+  },
+);
