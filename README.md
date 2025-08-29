5d19fbdb3db9014baa253a5b0821f6f44f0058d4
Hiroshi Urabe <mail@torounit.com>
2025-08-29 13:26:34 +0900

    feat: update eslint rule

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Vite + Tailwind CSS + React Template
 
 ## Tools
-* Vite
-* Tailwind CSS
-* React
-* TypeScript
-* eslint
-* prettier
-* Husky + lint-staged
+
+- Vite
+- Tailwind CSS
+- React
+- TypeScript
+- eslint
+- prettier
+- Husky + lint-staged
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 import { includeIgnoreFile } from "@eslint/compat";
 import eslint from "@eslint/js";
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 import eslintConfigPrettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 import { fileURLToPath } from "node:url";
@@ -11,9 +11,10 @@ const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
   eslintConfigPrettier,
-  reactHooks.configs['recommended-latest'],
+  reactHooks.configs["recommended-latest"],
   reactRefresh.configs.vite,
   includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
   {
@@ -22,6 +23,16 @@ export default tseslint.config(
   {
     languageOptions: {
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      "no-shadow": "off",
+      "@typescript-eslint/no-shadow": "error",
     },
   },
 );
