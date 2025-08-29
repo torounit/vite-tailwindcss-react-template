6a741f2159e4c578bb37d6c34a21b82773cd2eda
Hiroshi Urabe <mail@torounit.com>
2025-08-29 13:56:03 +0900

    refactor: fix eslint

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,5 +1,5 @@
 export default {
   "**/*.ts?(x)": () => "tsc -p tsconfig.json --noEmit",
-  "*.{js,ts,jsx,tsx}": "eslint --cache --fix",
   "*.{js,css,md,html}": "prettier --write",
+  "*.{js,ts,jsx,tsx}": "eslint --cache --fix",
 };
