5ab0d9366bb70da833e211c1c7dfdf05f29ed4ee
Hiroshi Urabe <mail@torounit.com>
2025-08-29 13:46:59 +0900

    feat: add rules for react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,12 @@
 import { includeIgnoreFile } from "@eslint/compat";
 import eslint from "@eslint/js";
 import globals from "globals";
+import eslintPluginReact from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import eslintConfigPrettier from "eslint-config-prettier";
-import perfectionist from 'eslint-plugin-perfectionist'
-import jsxA11y from 'eslint-plugin-jsx-a11y';
+import perfectionist from "eslint-plugin-perfectionist";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 import tseslint from "typescript-eslint";
 import { fileURLToPath } from "node:url";
 
@@ -15,10 +16,12 @@ export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
+  perfectionist.configs["recommended-natural"],
   jsxA11y.flatConfigs.recommended,
-  eslintConfigPrettier,
+  eslintPluginReact.configs.flat.recommended,
   reactHooks.configs["recommended-latest"],
   reactRefresh.configs.vite,
+  eslintConfigPrettier,
   includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
   {
     ignores: ["eslint.config.mjs"],
@@ -33,12 +36,12 @@ export default tseslint.config(
     },
   },
   {
-    plugins: { perfectionist: perfectionist }
-  },
-  {
     rules: {
       "no-shadow": "off",
       "@typescript-eslint/no-shadow": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
     },
   },
 );
